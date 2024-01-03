ed23a2f62c84e7eac4adc80430d21f096e0d8f42
ssSally111 <2392341633@qq.com>
2024-01-03 23:58:42 +0800

    update

--- h.h
+++ h.h
@@ -5,50 +5,31 @@ NTSYSAPI
 UCHAR*
 NTAPI
 PsGetProcessImageFileName(
-    IN  PEPROCESS process
+	IN  PEPROCESS process
 );
-
-EXTERN_C VOID Protection2(PCHAR procName);
 
 typedef struct _LDR_DATA_TABLE_ENTRY
 {
-    LIST_ENTRY64    InLoadOrderLinks;
-    LIST_ENTRY64    InMemoryOrderLinks;
-    LIST_ENTRY64    InInitializationOrderLinks;
-    PVOID            DllBase;
-    PVOID            EntryPoint;
-    ULONG            SizeOfImage;
-    UNICODE_STRING    FullDllName;
-    UNICODE_STRING     BaseDllName;
-    ULONG            Flags;
-    USHORT            LoadCount;
-    USHORT            TlsIndex;
-    PVOID            SectionPointer;
-    ULONG            CheckSum;
-    PVOID            LoadedImports;
-    PVOID            EntryPointActivationContext;
-    PVOID            PatchInformation;
-    LIST_ENTRY64    ForwarderLinks;
-    LIST_ENTRY64    ServiceTagLinks;
-    LIST_ENTRY64    StaticLinks;
-    PVOID            ContextInformation;
-    ULONG64            OriginalBase;
-    LARGE_INTEGER    LoadTime;
+	LIST_ENTRY64    InLoadOrderLinks;
+	LIST_ENTRY64    InMemoryOrderLinks;
+	LIST_ENTRY64    InInitializationOrderLinks;
+	PVOID            DllBase;
+	PVOID            EntryPoint;
+	ULONG            SizeOfImage;
+	UNICODE_STRING    FullDllName;
+	UNICODE_STRING     BaseDllName;
+	ULONG            Flags;
+	USHORT            LoadCount;
+	USHORT            TlsIndex;
+	PVOID            SectionPointer;
+	ULONG            CheckSum;
+	PVOID            LoadedImports;
+	PVOID            EntryPointActivationContext;
+	PVOID            PatchInformation;
+	LIST_ENTRY64    ForwarderLinks;
+	LIST_ENTRY64    ServiceTagLinks;
+	LIST_ENTRY64    StaticLinks;
+	PVOID            ContextInformation;
+	ULONG64            OriginalBase;
+	LARGE_INTEGER    LoadTime;
 } LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
-
-
-typedef struct _KSYSTEM_SERVICE_TABLE
-{
-    PULONG  ServiceTableBase;
-    PULONG  ServiceCounterTableBase;
-    ULONG   NumberOfService;
-    ULONG   ParamTableBase;
-} KSYSTEM_SERVICE_TABLE, * PKSYSTEM_SERVICE_TABLE;
-
-typedef struct _KSERVICE_TABLE_DESCRIPTOR
-{
-    KSYSTEM_SERVICE_TABLE   ntoskrnl;
-    KSYSTEM_SERVICE_TABLE   win32k;
-    KSYSTEM_SERVICE_TABLE   notUsed1;
-    KSYSTEM_SERVICE_TABLE   notUsed2;
-}KSERVICE_TABLE_DESCRIPTOR, * PKSERVICE_TABLE_DESCRIPTOR;
